f3b601393fcb92445f71c6948748acd040d9644f
nininini <jajanidzenino60@gmail.com>
2024-02-21 00:25:16 +0400

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+
+const menuBtn = document.querySelector(".humburger");
+const menu = document.querySelector(".full-menu");
+
+menuBtn.addEventListener("click", () => {
+  menu.classList.toggle("mobile-menu")
+  
+});
